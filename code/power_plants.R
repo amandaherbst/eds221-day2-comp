f2769bf2aa4afa3d2bed7f916151c05dc7fcd60e
amandaherbst <amandaeherbst@gmail.com>
2023-08-15 14:41:14 -0700

    r script power plants

--- code/power_plants.R
+++ code/power_plants.R
@@ -2,4 +2,18 @@
 
 library(tidyverse)
 library(here)
+library(janitor)
+
+plants <- read_csv(here("data", "power_plants.csv")) %>% 
+  clean_names()
+
+alabama_plants <- plants %>% 
+  filter(state_name == "Alabama" | state_name == "alabama") # only keeps plants in Alabama
+
+alabama_plot <- ggplot( data = alabama_plants,
+                        aes(x = install_mw,
+                            y = total_mw)) + 
+  geom_point()
+
+ggsave(here::here("figs", "alabama_plot.png"), height = 6, width = 7)
 
